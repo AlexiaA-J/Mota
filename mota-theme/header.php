3610ac15ecf59709256182fbdb5f5366c3141f77
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2023-12-04 12:48:00 +0100

    Add div navbar

--- mota-theme/header.php
+++ mota-theme/header.php
@@ -9,16 +9,16 @@
 <body>
     <?php wp_body_open(); ?>
     <header>
-        <div id="site-logo">
-            <?php
-                if ( get_theme_mod( 'your_theme_logo' ) ) : ?>
-                    <a href="<?php echo home_url(); ?>">
-                        <img class=logo src="<?php echo get_theme_mod( 'your_theme_logo' ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" >
-                    </a>
-            <?php //
-                else : ?>
-                    <h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
-            <?php endif; 
-            ?>
+        <?php
+            if ( get_theme_mod( 'your_theme_logo' ) ) : ?>
+            <a href="<?php echo home_url(); ?>">
+                <img class=logo src="<?php echo get_theme_mod( 'your_theme_logo' ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" >
+            </a>
+        <?php //
+            else : ?>
+                <h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
+        <?php endif; 
+        ?>
+        <div class="navBar">
+            <?php wp_nav_menu(array('header-menu' => 'header-menu'))?>
         </div>
-         <?php wp_nav_menu(array('header-menu' => 'header-menu'))?>
