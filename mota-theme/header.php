96011660666ed6992ade2ec0e62d9487158f466b
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2023-12-04 10:55:34 +0100

    Add logo and header menu

--- mota-theme/header.php
+++ mota-theme/header.php
@@ -4,5 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nathalie Mota - Photographe Event</title>
+    <?php wp_head(); ?>
 </head>
 <body>
+    <?php wp_body_open(); ?>
+    <header>
+        <div id="site-logo">
+            <?php
+                if ( get_theme_mod( 'your_theme_logo' ) ) : ?>
+                    <a href="<?php echo home_url(); ?>">
+                        <img class=logo src="<?php echo get_theme_mod( 'your_theme_logo' ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" >
+                    </a>
+            <?php //
+                else : ?>
+                    <h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
+            <?php endif; 
+            ?>
+        </div>
+         <?php wp_nav_menu(array('header-menu' => 'header-menu'))?>
