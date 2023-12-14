0e8de08674717f2a6f31b9e27d669fff44b1c606
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2023-12-14 11:35:06 +0100

    Remove error lines

--- mota-theme/assets/js/scripts.js
+++ mota-theme/assets/js/scripts.js
@@ -1,19 +1,16 @@
-// MENU NAV
+// MENU BURGER
 
-document.addEventListener("DOMContentLoaded", function() {
-    const menuBurger = document.getElementById("menu_burger");
-    const openBtn = document.getElementById("openBtn");
-    const closeBtn = document.getElementById("closeBtn");
+const menuBurger = document.getElementById("menu_burger");
+const openBtn = document.getElementById("openBtn");
+const closeBtn = document.getElementById("closeBtn");
 
-    openBtn.onclick = openMenu;
-    closeBtn.onclick = closeMenu;
+openBtn.onclick = openMenu;
+closeBtn.onclick = closeMenu;
 
-    function openMenu() {
-        menuBurger.classList.add("active");
-        console.log("Coucou");
-    }
+function openMenu() {
+    menuBurger.classList.add("active");
+}
 
-    function closeMenu() {
-        menuBurger.classList.remove("active");
-    }
-});
+function closeMenu() {
+    menuBurger.classList.remove("active");
+}
