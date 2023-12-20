ac3351e9df1447f47600956d3bb6e8e86fd6446d
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2023-12-20 10:11:41 +0100

    Add modal opening/close functions

--- mota-theme/assets/js/scripts.js
+++ mota-theme/assets/js/scripts.js
@@ -1,16 +1,46 @@
-// MENU BURGER
+// DOMContentLoaded related functions
 
-const menuBurger = document.getElementById("menu_burger");
-const openBtn = document.getElementById("openBtn");
-const closeBtn = document.getElementById("closeBtn");
+document.addEventListener("DOMContentLoaded", function() {
 
-openBtn.onclick = openMenu;
-closeBtn.onclick = closeMenu;
+    // Menu nav burger
 
-function openMenu() {
-    menuBurger.classList.add("active");
-}
+    const menuBurger = document.getElementById("menu_burger");
+    const openBtn = document.getElementById("openBtn");
+    const closeBtn = document.getElementById("closeBtn");
 
-function closeMenu() {
-    menuBurger.classList.remove("active");
-}
+    openBtn.onclick = openMenu;
+    closeBtn.onclick = closeMenu;
+
+    function openMenu() {
+        menuBurger.classList.add("active");
+    }
+
+    function closeMenu() {
+        menuBurger.classList.remove("active");
+    }
+
+    // Contact form
+
+    const contactBtns = document.querySelectorAll(".menu-item-29 a");
+    const modalForm = document.querySelector(".modal-overlay");
+    const modalContent = document.getElementById("wpcf7-f28-o1");
+
+    contactBtns.forEach(contactBtn => {
+        contactBtn.onclick = openForm;
+    });
+
+    function openForm(event) {
+        event.preventDefault();
+        modalForm.classList.add("active");
+
+        document.addEventListener("click", closeFormOutside);
+    }
+
+    function closeFormOutside(event) {
+        if (!modalContent.contains(event.target) && !Array.from(contactBtns).includes(event.target)) {
+            modalForm.classList.remove("active");
+
+            document.removeEventListener("click", closeFormOutside);
+        }
+    }
+});
