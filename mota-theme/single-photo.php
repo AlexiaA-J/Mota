f001dbfbaba4f1a27672857e0f7a142e694d04aa
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2023-12-22 11:20:23 +0100

    Add new single and general idea of its content

---
+++ mota-theme/single-photo.php
@@ -0,0 +1,18 @@
+<?php
+get_header();
+?>
+    <div class="page-content">
+    <!-- Section Superieur -->
+        <section class="main-content">
+            <div class="upper-content">
+                <div class="infos-container">
+                    
+                </div>
+                <div class="photos-container">
+                </div>
+            </div>
+        </section>
+    </div>    
+<?php
+get_footer();
+?>
