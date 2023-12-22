ace9a1c6b954923d579a4dd490b15eabf7bd1ef5
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2023-12-22 11:29:21 +0100

    Add html layout of upper section content

--- mota-theme/single-photo.php
+++ mota-theme/single-photo.php
@@ -6,9 +6,15 @@ get_header();
         <section class="main-content">
             <div class="upper-content">
                 <div class="infos-container">
-                    
+                    <p class=photo-title><?php echo get_the_title()?></p>
+                    <p> Référence : <span class="ref-value"><?php /*Add php content here */ ?></span><p>
+                    <p> Catégorie : <?php /*Add php content here */ ?></p>
+                    <p> Format : <?php /*Add php content here */ ?></p>
+                    <p> Type : <?php /*Add php content here */ ?></p>
+                    <p> Année : <?php /*Add php content here */ ?></p>
                 </div>
                 <div class="photos-container">
+                    <img src="<?php /*Add php content here */?>">
                 </div>
             </div>
         </section>
