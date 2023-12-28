4329da789b0f2f72720807e8dfdeac56d04ea914
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2023-12-28 08:53:52 +0100

    Add contact div

--- mota-theme/single-photo.php
+++ mota-theme/single-photo.php
@@ -5,28 +5,30 @@ get_header();
         <div class="page-content">
         <!-- Section Superieur -->
             <section class="main-content">
-                <div class="upper-content">
+                <div class="single-photo-content">
                     <div class="infos-container">
                         <p class=photo-title><?php echo get_the_title()?></p>
                         <p> Référence : <span class="ref-value"><?php echo get_field('ref');?></span></p>
-                        <p> Catégorie : <?php $categories = get_the_terms( $post->ID, 'category' );
-                                        foreach( $categories as $category ) {
-                                        echo $category->name;
-                                        } ?></p>
-                        <p> Format : <?php $formats = get_the_terms( $post->ID, 'format' );
-                                        foreach( $formats as $format ) {
-                                        echo $format->name;
-                                        } ?></p>
+                        <p> Catégorie : <?php display_taxonomy_terms($post->ID, 'category'); ?></p>
+                        <p> Format : <?php display_taxonomy_terms($post->ID, 'format'); ?></p>
                         <p> Type : <?php echo get_field('type'); ?></p>
-                        <p> Année : <?php $dates = get_the_terms( $post->ID, 'date' );
-                                        foreach( $dates as $date ) {
-                                        echo $date->name;
-                                        } ?></p>
+                        <p> Année : <?php display_taxonomy_terms($post->ID, 'date'); ?></p>
+                        <div class="line">    
+                            <hr>
+                        </div>
                     </div>
                     <div class="photos-container">
                         <img src="<?php $photo = get_field('photo');
                                         echo $photo['url'];
                                     ?>" alt="Photographie">
+                    </div>
+                </div>
+                <div class="contact-content">
+                    <div class="contact">
+                        <p class="contact-text">Cette photo vous intéresse ?</p>
+                        <button class="contact-btn">Contact</button>
+                    </div>
+                    <div class="preview">
                     </div>
                 </div>
             </section>
