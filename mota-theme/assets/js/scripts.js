f4aaf380527287bf68d373e8cc0073b79f6bda32
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2023-12-28 16:28:51 +0100

    Update contact form function

--- mota-theme/assets/js/scripts.js
+++ mota-theme/assets/js/scripts.js
@@ -20,27 +20,40 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     // Contact form call from menu
+const contactBtns = document.querySelectorAll(".menu-item-29 a, .contact-btn");
+const modalForm = document.querySelector(".modal-overlay");
+const modalContent = document.getElementById("wpcf7-f28-o1");
+const formRefDiv = document.querySelector(".formRef");
 
-    const contactBtns = document.querySelectorAll(".menu-item-29 a");
-    const modalForm = document.querySelector(".modal-overlay");
-    const modalContent = document.getElementById("wpcf7-f28-o1");
+contactBtns.forEach(contactBtn => {
+    contactBtn.onclick = openForm;
+});
 
-    contactBtns.forEach(contactBtn => {
-        contactBtn.onclick = openForm;
-    });
+function openForm(event) {
+    event.preventDefault();
+    const refValueElement = document.querySelector(".ref-value");
 
-    function openForm(event) {
-        event.preventDefault();
-        modalForm.classList.add("active");
+    // Check if ref-value element is present on the page
+    if (refValueElement) {
+        const refValue = refValueElement.textContent;
 
-        document.addEventListener("click", closeFormOutside);
-    }
-
-    function closeFormOutside(event) {
-        if (!modalContent.contains(event.target) && !Array.from(contactBtns).includes(event.target)) {
-            modalForm.classList.remove("active");
-
-            document.removeEventListener("click", closeFormOutside);
+        const inputField = formRefDiv.querySelector("input[name='your-subject']");
+        if (inputField) {
+            inputField.value = refValue;
         }
+
+        const uppercaseValue = refValue.toUpperCase();
+
+        inputField.value = uppercaseValue;
     }
+    modalForm.classList.add("active");
+        document.addEventListener("click", closeFormOutside);
+}
+
+function closeFormOutside(event) {
+    if (!modalContent.contains(event.target) && !Array.from(contactBtns).includes(event.target)) {
+        modalForm.classList.remove("active");
+        document.removeEventListener("click", closeFormOutside);
+    }
+}
 });
