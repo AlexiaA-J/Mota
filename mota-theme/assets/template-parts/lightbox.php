0da7d01dadba326dd1adcfeefe700f0de9d4cd25
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2024-01-24 12:24:01 +0100

    Correct class

--- mota-theme/assets/template-parts/lightbox.php
+++ mota-theme/assets/template-parts/lightbox.php
@@ -1,7 +1,7 @@
 <div class="lightbox-container">
     <div id="lightbox" class="lightbox">
        <div class="lightbox__content">
-            <span class="lightbox__categorie"></span>
+            <span class="lightbox__category"></span>
             <span class="lightbox__ref"></span>          
             <img class="lightbox__close" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/lightbox_crossmark.png'; ?>" alt="croix">
             <span class="lightbox__prev">&#8592; Précédente</span>
