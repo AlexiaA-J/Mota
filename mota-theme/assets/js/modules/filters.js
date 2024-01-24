732c4053f5362decb7a2f011d838f8a4a4e05e21
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2024-01-24 12:25:07 +0100

    Clean js code

---
+++ mota-theme/assets/js/modules/filters.js
@@ -0,0 +1,56 @@
+// FILTERS
+
+function initializeFilters() {
+    var activeCategory = 'all';
+    var activeFormat = 'all';
+    var activeSortByDate = 'all';
+
+    jQuery('#categories').val(activeCategory);
+    jQuery('#formats').val(activeFormat);
+    jQuery('#sort-by-date').val(activeSortByDate);
+
+    function areFiltersActive() {
+        return activeCategory !== 'all' || activeFormat !== 'all' || activeSortByDate !== 'all';
+    }
+
+    // Event handler for filter changes
+    jQuery('#categories, #formats, #sort-by-date').on('change', function() {
+        ajaxFilter();
+        checkIfMorePosts();
+    });
+
+    function ajaxFilter() {
+        var category = jQuery('#categories').val();
+        var format = jQuery('#formats').val();
+        var sortByDate = jQuery('#sort-by-date').val();
+
+        // Update active filter states
+        activeCategory = category;
+        activeFormat = format;
+        activeSortByDate = sortByDate;
+
+        // Hide the "Load More" button if filters are active
+        if (areFiltersActive()) {
+            jQuery('#load-more').hide();
+        }
+
+        jQuery.ajax({
+            type: 'POST',
+            url: './wp-admin/admin-ajax.php',
+            data: {
+                action: 'ajax_filter',
+                category: category,
+                format: format,
+                sortByDate: sortByDate
+            },
+            success: function(response) {
+                jQuery('.gallery-container').html(response);
+
+                // Show the "Load More" button if no filters are active
+                if (!areFiltersActive()) {
+                    jQuery('#load-more').show();
+                }
+            }
+        });
+    }
+}
