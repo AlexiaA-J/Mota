75903659b9b898a942efc628b43c9056f02fa93a
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2024-01-25 09:14:21 +0100

    Add minimal style to pages

--- mota-theme/page.php
+++ mota-theme/page.php
@@ -3,8 +3,9 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
-
-		
+		<div class="pages-content">
+			<?= get_post_field('post_content', $post->ID) ?>
+		</div>
 	</main><!-- #main -->
 
 <?php
